8d71ea230320b7150f5d4a78553a8ce425a55497
KrNel <kris.b.nelson@gmail.com>
2021-01-16 20:47:18 -0500

    fix: add link on article avatar

--- html/layouts/joomla/content/info_block.php
+++ html/layouts/joomla/content/info_block.php
@@ -15,38 +15,57 @@ $db = Factory::getDbo();
 
 $url = JUri::current();
 $showAvatar = 0;
-$avatar = '';
 $showArticleBodyLine = 0;
+$avatarURL = '';
+$catAlias = '';
+$articleId = '';
+$assetId = '';
 
-if (!preg_match('/(about|all|all-content|creators|donate|featured|schedule)$/', $url)) {
-  $showAvatar = 1;
-	$showArticleBodyLine = 1;
-}
+/*echo "<pre>";
+var_dump($displayData);
+echo "</pre>";*/
 
 $blockPosition = $displayData['params']->get('info_block_position', 0);
 
-if ($showAvatar !== 0):
+if (!preg_match('/(about|all|all-content|creators|donate|featured|schedule)$/', $url)) {
+
+  $showAvatar = 1;
+	$showArticleBodyLine = 1;
 	$articleId = $displayData['item']->id;
+	if (property_exists($displayData['item'], "asset_id")):
+		$assetId = $displayData['item']->asset_id;
+	endif;
+}
 
-	$db->setQuery(
-		'SELECT up.profile_value
-		FROM #__content c, #__users u, #__user_profiles up
-		WHERE c.created_by = u.id AND u.id = up.user_id AND up.profile_key = ' . $db->quote("profile.avatar") . ' AND c.id = ' . $articleId
-	);
+if ($showAvatar !== 0):
+	if (!empty($assetId)):
+		// ID of article in _content.id (63)
+		// Asset ID of article in _content.asset_id (261)
+		// where _content.created_by = _users.id (301)
+		// where _users.id = _user_profiles.user_id
+		// where _user_profiles.profile_key = profile.avatar
+		// get _user_profiles.profile_value
+		$sql = 'SELECT up.profile_value, ca.alias
+		FROM jospn_content c
+			INNER JOIN jospn_users u ON c.created_by = u.id
+			INNER JOIN jospn_user_profiles up ON u.id = up.user_id,
+			jospn_assets a
+				INNER JOIN jospn_categories ca ON a.parent_id = ca.asset_id
+		WHERE up.profile_key = ' . $db->quote("profile.avatar") . ' AND c.id = ' . $articleId . ' AND a.id = ' . $assetId;
 
-	$result = $db->loadObjectList();
-	$avatar = str_replace('"', "", $result[0]->profile_value);
+		$db->setQuery($sql);
+
+		$result = $db->loadObjectList();
+		$avatarURL = stripslashes(str_replace('"', "", $result[0]->profile_value));
+		$catAlias = str_replace('"', "", $result[0]->alias);
+	endif;
 endif;
-
-/*echo "<pre>";
-print_r($this);
-echo "</pre>";*/
 ?>
 	<dl class="article-info muted">
 
 		<?php
-			if ($showAvatar === 1):
-				$img = '<dd class="avatar-article-info"><img src="' . $avatar . '" /></dd>';
+			if (!empty($assetId) && $showAvatar !== 0):
+				$img = '<dd class="avatar-article-info"><a href="' . $catAlias . '"><img src="' . $avatarURL . '" /></a></dd>';
 				echo $img;
 				echo '<div class="article-info-text">';
 			endif;
@@ -81,7 +100,7 @@ echo "</pre>";*/
 			<?php if ($displayData['params']->get('show_publish_date')) : ?>
 				<?php
 					$date = $this->sublayout('publish_date', $displayData);
-					if ($showAvatar === 1):
+					if ($showAvatar !== 0):
 						echo str_replace('•', "", $date);
 					else:
 						echo $date;
@@ -108,7 +127,7 @@ echo "</pre>";*/
 		<?php endif; ?>
 
 		<?php
-			if ($showAvatar === 1):
+			if ($showAvatar !== 0):
 				echo '</div>';
 			endif;
 		?>
